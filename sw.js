6e6440b174f220e9beabddcfef50e55ced2a89e2
LOLandXD <13585566117@139.com>
2026-07-25 09:39:03 +0800

    chore: bump service worker cache version to force refresh updated HTML

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const APP_SHELL_CACHE = "new-england-four-app-v67";
-const DATA_CACHE = "new-england-four-data-v67";
+const APP_SHELL_CACHE = "new-england-four-app-v68";
+const DATA_CACHE = "new-england-four-data-v68";
 const DATA_CACHE_TTL_MS = 24 * 60 * 60 * 1000;
 const APP_SHELL_FILES = [
   "/",
