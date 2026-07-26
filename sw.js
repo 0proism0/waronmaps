84e26393bbdac3c2b72917070a997e311eb68819
LOLandXD <13585566117@139.com>
2026-07-26 20:01:01 +0800

    ui: remove top menu button, keep send value/mode on node select, icons+explanations for Fixed/Relative

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const APP_SHELL_CACHE = "new-england-four-app-v77";
-const DATA_CACHE = "new-england-four-data-v77";
+const APP_SHELL_CACHE = "new-england-four-app-v78";
+const DATA_CACHE = "new-england-four-data-v78";
 const DATA_CACHE_TTL_MS = 24 * 60 * 60 * 1000;
 const APP_SHELL_FILES = [
   "/",
