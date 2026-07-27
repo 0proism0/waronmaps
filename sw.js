710423c4354a65f1aca05e251a8e50661a495af0
LOLandXD <13585566117@139.com>
2026-07-27 20:58:04 +0800

    feat: dismissible start-location intro + confirm dialog on node click; CrazyGames SDK external cloud save

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const APP_SHELL_CACHE = "new-england-four-app-v91";
-const DATA_CACHE = "new-england-four-data-v91";
+const APP_SHELL_CACHE = "new-england-four-app-v92";
+const DATA_CACHE = "new-england-four-data-v92";
 const DATA_CACHE_TTL_MS = 24 * 60 * 60 * 1000;
 const APP_SHELL_FILES = [
   "/",
