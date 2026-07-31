b21204833ac1d47670e4d83e6ad206ac63c78cce
LOLandXD <13585566117@139.com>
2026-07-31 09:52:38 +0800

    i18n: translate road names to selected language (road-type terms); delete redundant Node row from info panel

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const APP_SHELL_CACHE = "new-england-four-app-v103";
-const DATA_CACHE = "new-england-four-data-v103";
+const APP_SHELL_CACHE = "new-england-four-app-v104";
+const DATA_CACHE = "new-england-four-data-v104";
 const DATA_CACHE_TTL_MS = 24 * 60 * 60 * 1000;
 const APP_SHELL_FILES = [
   "/",
